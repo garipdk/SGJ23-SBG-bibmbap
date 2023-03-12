698ab8267f8d30f9a7b7f7bdd62db7f56adb9639
Garip Windows <gkusoglu@etu.unistra.fr>
2023-03-12 14:46:29 +0100

    add somme descriptions

--- scripts-gdscript/hypertexte_texts.gd
+++ scripts-gdscript/hypertexte_texts.gd
@@ -1,12 +1,76 @@
 extends Node
 
-
 var description = {
 	"chapeau":{
 		"title":"Chapeau",
 		"text":"Voici un [url=petit]petit[/url] Chapeau.",
 		"size":100
-	}
+	},
+	"titre_proprio":{
+		"title":"Objet : Titre de propriété",
+		"text": "Madame Lee Sonya,\n"\
+		+ "\n"\
+		+ "Cette lettre fait office de titre de propriété officiel.\n"\
+		+ "Cette propriété est inamovible, et ne peut être cédée à moins que le décès du propriétaire ne soit constaté. Dans ce cas, les règles de successions s’appliquent, et il faudra alors se référer au testament du défunt.\n"\
+		+ "Aucune contestation ne peut être soumise.\n"\
+		+ "\n"\
+		+ "Maître Kim Gyu-bok",
+		"size":100
+	},
+	"testament":{
+		"title":"Ceci est mon testament",
+		"text":"Je soussigné Lee Yeong-ho, né le 16 février 1920 à Busan, dispose en dernière volonté :\n"\
+		+ "    1. Ayant des soupçons quant à la loyauté de mon épouse, j’annule ici tous mes testaments antérieurs\n"\
+		+ "    2. Je lègue tous mes biens à mes enfants Lee Sun-na, Lee Yun-bok et Lee Tae-sun à parts égales.\n"\
+		+ "Lee Yeong-ho, le 20 avril 1951",
+		"size":100
+	},
+	"telegramme":{
+		"title":"Avis de décès",
+		"text":"Madame,\n"\
+		+ "C’est avec le plus grand regret que nous vous annonçons le décès de Lee Yeong-ho, soldat de l’armée de la République de Corée, tombé au combat le 15 juin 1951.\n"\
+		+ "Nous vous présentons nos sincères condoléances, et vous assurons que son sacrifice héroïque ne tombera pas dans l’oubli.\n",
+		"size":100
+	},
+	"lettre_front":{
+		"title":"Lettre d’amour de Lee Yeong-ho à Lee Sonya depuis le front :",
+		"text":"Le 2 juillet 1950\n"\
+		+ "\n"\
+		+ "Mon amour, mon épouse,\n"\
+		+ "\n"\
+		+ "Cela fait désormais presque un mois que la guerre a commencé. Je n’arrive pas à croire que nous ayons été si brutalement séparés, à peine deux mois après notre mariage. Ici, sur le front, la vie est difficile, mais j’ai la chance d’avoir mon grand-frère à mes côtés. Nous perdons du terrain de jour en jour.\n"\
+		+ "Le visage de Choon-ho me hante. Sur le champ de bataille, j’ai l’impression de le voir partout, depuis qu’il a rejoint les rouges… A chaque fois que je tire, j’ai l’impression de le tuer, et de me briser.\n"\
+		+ "Le Capitaine a promis de m’accorder une permission le mois prochain. Il me tarde de vous revoir, toi et les enfants.\n"\
+		+ "\n"\
+		+ "Je vous embrasse tendrement,\n"\
+		+ "\n"\
+		+ "Yeong-ho\n"\
+		+ "\n"\
+		+ "PS : Je te joins une photo de moi et de Cheol-ho, prise la semaine dernière.\n",
+		"size":100
+	},
+	"journal":{
+		"title":"Page de journal intime de Sonya :",
+		"text":"04/03/1951\n"\
+		+ "La journée a été merveilleuse ! Michael m’a donné rendez-vous dans un café de style occidental. Nous avons partagé une part de gâteau au chocolat, accompagnée d’un grand café pour moi, et d’un whisky pour lui. Michael m’a encore offert un cadeau : un collier de perles ! Il a promis de m’emmener au cinéma, demain, ils repassent des films de Chaplin ! A ce rythme, je ne vais plus pouvoir me contenter de la vie domestique, la vie urbaine a trop à offrir…\n"\
+		+ "Malheureusement, j’ai dû écourter notre moment pour amener Sun-na à son cours de piano. J’avais complètement oublié ce détail, quand j’ai fixé l’heure du rendez-vous… Sur le moment, cela m’a mise hors de moi, ce sont les enfants de Yeong-ho, pas les miens, mais ensuite, je me suis sentie bête et égoïste.\n"\
+		+ "\n"\
+		+ "Je pense de moins en moins à lui.\n",
+		"size":100
+	},
+	"lettre_michael":{
+		"title":"Lettre à Michael :",
+		"text":"Le 17 octobre 1953\n"\
++"\n"\
++"Hi Darling,\n"\
++"\n"\
++"La décision est prise. Je vais trouver un moyen de garder la maison, et tu pourras t’y installer à mes côtés, maintenant que la guerre est terminée. Mon amie Ok-ran m’a parlé d’un avocat, qui peut me fournir un titre de propriété à mon nom. Reste à se débarrasser des enfants… Je sais que c’est injuste, mais c’est tout aussi injuste de me priver de ma liberté en me les laissant dans les pattes. Eux aussi, seront plus heureux chez quelqu’un qui sera capable de les aimer, je suppose.\n"\
++"\n"\
++ "I love you.\n"\
++"\n"\
++"Sonya\n",
+		"size":100
+	},
 }
 
 var item_description = {#pour hypertxt
